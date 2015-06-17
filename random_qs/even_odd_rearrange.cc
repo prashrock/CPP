1412c5c80eb415493b4d83cf82e2935e2996d9c0
Prashanth Kannan <prashrock@gmail.com>
2015-06-16 17:10:43 -0700

    even_odd_rearrange 06/16/2015

--- random_qs/even_odd_rearrange.cc
+++ random_qs/even_odd_rearrange.cc
@@ -1,4 +1,9 @@
 //g++ -std=c++11 -o tst tst.cc
+/* Given the array of integers containing equal number of even *
+ * and odd numbers. Rearrange the array such that even number  *
+ * is at even place and odd number is at odd place.            *
+ * Example : [2,1,3,4,7,9,24,98]                               *
+ * Answer  : 1,2,3,4,7,24,9,98                                 */
 #include <iostream>          /* std::cout        */
 #include <iomanip>           /* std::setw        */
 #include <cmath>             /* pow              */
