bcea182f29d331803220f72729c712e5c31f9340
Prashanth Kannan <prashrock@gmail.com>
2016-07-22 07:25:17 -0700

    Leet: Fix comment in DP Easy Robber 07/22/2016

--- leetcode/algo_dp_easy_house_robber.cc
+++ leetcode/algo_dp_easy_house_robber.cc
@@ -5,18 +5,6 @@
  * @brief Given list of integers find max (not chosing 2 contiguous)
  */
 
-/**
- * You are a professional robber planning to rob houses along a street.
- * Each house has a certain amount of money stashed, the only constraint
- * stopping you from robbing each of them is that adjacent houses have
- * security system connected and it will automatically contact the police
- * if two adjacent houses were broken into on the same night.
-
- * Given a list of non-negative integers representing the amount of money
- * of each house, determine the maximum amount of money you can rob tonight
- * without alerting the police.
- */
-
 // https://leetcode.com/problems/house-robber/
 
 #include <iostream>          /* std::cout                    */
@@ -28,6 +16,18 @@
 #include <cstring>           /* std::strtok                  */
 #include <unordered_map>     /* std::unordered_map container */
 using namespace std;
+
+/**
+ * You are a professional robber planning to rob houses along a street.
+ * Each house has a certain amount of money stashed, the only constraint
+ * stopping you from robbing each of them is that adjacent houses have
+ * security system connected and it will automatically contact the police
+ * if two adjacent houses were broken into on the same night.
+
+ * Given a list of non-negative integers representing the amount of money
+ * of each house, determine the maximum amount of money you can rob tonight
+ * without alerting the police.
+ */
 
 int rob(vector<int>& nums) {
    vector<int> dp(nums.size());
