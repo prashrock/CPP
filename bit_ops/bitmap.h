db6e749b2abc75f3c3642ef42099a08b91db3011
Prashanth Kannan <prashrock@gmail.com>
2016-07-25 08:33:46 -0700

    Simple resizable Bitmap implementation 07/25/2016

--- bit_ops/bitmap.h
+++ bit_ops/bitmap.h
@@ -2,142 +2,96 @@
 #define _BITMAP_CPP_
 
 #include <algorithm>           /* std::max                   */
-#include <cstring>             /* size_t                     */
-#include <cstdint>             /* uint64_t                   */
+#include <cstring>             /* memset                     */
 #include <climits>             /* CHAR_BIT                   */
-#include <cerrno>              /* CHAR_BIT                   */
-static const int u64_bit = (CHAR_BIT * sizeof(uint64_t));
+#include <bitset>              /* std::bitset (dump)         */
 
-/* Constant size bitmap definition                           *
- * Note: valid entries for bitmap = [0 - N]. N should be     *
- * specified during bitmap creation.                         */
-class bitmap
+/* Simple resizable Bitmap to hold [0, max_n] bits. Uses     *
+ * STL Vector as Container and **not thread safe**           *
+ * Todo:                                                     *
+ * - Feature: For sparse bitmaps, use hierarchical 2D bitmap *
+ * - Perf: Avoid divide and modulo operations                *
+ * - Perf: Operate with cacheline sized chunks               */
+class Bitmap
 {
 public:
-	/* Create bitmap based on max capacity [0-max_n]         */
-	void init(size_t max_n){
-		length = 0;
-		if(max_n >= u64_bit) {
-			length = (max_n / u64_bit) + 1;
-			words  = new uint64_t[length];
-		}
-		clear();
-	}
-	/* Reset the bitmap                                      */
-	void clear() {
-		if(!length) bits = 0;
-		else memset(words, 0, length * sizeof(uint64_t));
-	}
-	/* Set all the bits in the bitmap                        */
-	void set_all() {
-		if(!length) bits = -1;
-		else memset(words, -1, length * sizeof(uint64_t));
-	}
-	/* Constructor for the bitmap class                      *
-	 * @param max_n - maximum value in this static bitmap    */
-    bitmap(size_t max_n) : length(0), bits(0) {
-		init(max_n);
-	}
-	/* Destructor for the bitmap class                       */
-	~bitmap() {
-		if(length && words) delete[] words;
-	}
-	/* Bitmap class get API                                  *
-	 * @param pos - position idx. Can be zero/one based      *
-	 * Note: As long as user is consistent with get/set API  *
-	 * Bitmap Class does not care if idx is 0 or 1 based     */
-	bool get(size_t pos) {
-		uint64_t *word = get_word(pos);
-		if(!word) return false;
-		else      return ((*word & wordmask(pos)) != 0);
-	}
-	/* Bitmap class get next set position idx                *
-	 * @param pos - position idx. Can be zero/one based      *
- 	 * @param next_pos - reference variable for next_pos     */
-	bool get_next_set_pos(size_t pos, size_t &next_pos) {
-		next_pos = pos + 1;
-		/* If next pos is not valid, nothing to do           */
-		uint64_t *word = get_word(next_pos); /*Get word      */
-		if(!word) return false;
-		/* If lucky, cur_word has nextsetbit, check cur_word */
-		int ffs = bit_ffs(*word >> (next_pos % u64_bit));
-		if(ffs >= 0)  {
-			next_pos += ffs;
-			return true;
-		}
-		/* Start from next word till length & find_first_bit */
-		for(auto i = get_word_idx(next_pos)+1; i < length; ++i) {
-			ffs = bit_ffs(words[i]);
-			if(ffs >= 0)  {
-				next_pos = i * u64_bit + ffs;
-				return true;
-			}
-		}
-		return false;
-	}
-	/* Set Bitmap at specified position                      *
-	 * @param pos - position idx. Can be zero/one based      */
-	void set(size_t pos)   {set(pos, true);}
-	/* Clear/Reset Bitmap at specified position              *
- 	 * @param pos - position idx. Can be zero/one based      */
-	void reset(size_t pos) {set(pos, false);}
-	
+   /* Create bitmap based on max capacity [0-max_n]          *
+    * @param max_n - maximum value in this static bitmap     */
+   Bitmap(size_t max_n) : bw((max_n >> u64_shift) + 1 , 0)      {       }
+   ~Bitmap() { }    /* Destructor for the bitmap class       */
+   /* Check if a given position is valid within bitmap       */
+   bool isValid(size_t pos) const { return get_word_idx(pos)<bw.size(); }
+   /* Bitmap Set operations (set, set vector, set all)       */
+   void set(size_t pos)            { set(pos, true);                    }
+   void set(const std::vector<size_t>& pv) { for(auto p : pv) set(p);   }
+   void setAll() { std::fill(bw.begin(), bw.end(), (uint64_t) - 1);     }
+   /* Bitmap Reset operations (reset,reset vector, clear all)*/
+   void reset(size_t pos)          { set(pos, false);                   }
+   void reset(const std::vector<size_t>& pv) { for(auto p:pv) reset(p); }
+   void reset()         { std::fill(bw.begin(), bw.end(),  0);          }
+   /* Bitmap Size ops (container size, #set bits, is empty? */
+   inline size_t size()  const { return (bw.size() << u64_shift);       }
+   inline size_t count() const { return cnt();                          }
+   inline bool isEmpty() const { return (count() == 0);                 }
+   inline void resize(size_t n){ bw.resize((n >> u64_shift) + 1 , 0);   }
+   /* Bitmap class get API (get if a bit is set or not)      */
+   bool get(size_t pos) const {
+      if(isValid(pos) == false) return false;
+      else return ((bw[get_word_idx(pos)] & wordmask(pos)) != 0);
+   }
+   /* Bitmap class get next set position idx                 *
+    * @param pos - position idx. Can be zero/one based       *
+    * @param next_pos - reference variable for next_pos      */
+   bool get_next_set_pos(size_t pos, size_t &next_pos) {
+      if(isValid(pos+1) == false) return false; /* not last chk */
+      uint64_t mask = ~bit_propagate_ones_right(wordmask(pos));
+      for(auto i = get_word_idx(pos); i < bw.size(); ++i, mask = -1) {
+         int ffs = bit_ffs(bw[i] & mask); /* 1st set bit pos    */
+         if(ffs >= 0)  { /* If there is a bit, calc global idx  */
+            next_pos = (i << u64_shift) + ffs;
+            return true;
+         }
+      }
+      return false;
+   }
+   void dump() const { for(auto v:bw) cout<<std::bitset<u64_bit>(v)<<endl; }
 private:
-	/* Use GCC built-in FFS. Note, returns 1 based idx       */
-	int bit_ffs(uint64_t word) {
-		return __builtin_ffsll(word) - 1;
-	}
-		
-	/* Clear the Lowest set bit of given word                */
-	uint64_t clear_lsb(uint64_t n) {
-		return (n & (n - 1));
-	}
-	/* Get the Lowest set bit of given word(C built-in)      */
-	uint64_t get_lsb_word_pos(uint64_t n) {
-		return __builtin_ctzll(n);
-	}
-	/* Given position [0-n], find the 64-bit word index      *
-	 * which contains this position.                         *
-	 * For 64-bit bitmap, this always returns 0              */
-	size_t get_word_idx(size_t pos)
-	{
-		return (length) ? pos/u64_bit : 0;
-	}
-	/* Given position [0-n) find 64-bit word with this bit   */
-	uint64_t *get_word(size_t pos) {
-		if(!length)                    return &bits;
-		else if(length > pos/u64_bit)  return &words[pos/u64_bit];
-		else                           return nullptr;
-	}
-	/* Given position, given next word                       */
-	uint64_t *get_next_word(size_t pos) {
-		if(!length)                return nullptr;
-		else {
-			size_t next_word       = get_word_idx(pos) + 1;
-			if(next_word < length) return &words[next_word];
-			else                   return nullptr;
-		}
-	}
-	/* Given a bit position [0-63) create a mask             */
-	uint64_t wordmask(size_t bit) {
-		return ((uint64_t)1 << (bit % u64_bit));
-	}
-	/* Given position[0-n), set/clear bit in that position   */
-	void set(size_t bit, bool on) {
-		size_t bitmap_size = length ? (length * u64_bit) : u64_bit;
-		if(bit >= bitmap_size) return;
-		uint64_t *word = get_word(bit);
-		if(on)
-			*word |= wordmask(bit);
-		else
-			*word &= ~wordmask(bit);
-	}
-	/* Bitmap class member variables                         */
-    size_t length;
-    union {
-	    uint64_t bits;
-	    uint64_t *words;
-    };
+   /* Bitmap class member variables                          */
+   std::vector<uint64_t> bw; /* Underlying bitmap container  */
+   const static int u64_bit   = (CHAR_BIT * sizeof(uint64_t));
+   const static int u64_shift = 6; /* use 2^6 = 64, no divide*/ 
+
+   /* Use GCC built-in Find First Set (which is 1 based idx) */
+   inline int bit_ffs(uint64_t w) const { return __builtin_ffsll(w)-1;   }
+   /* Use GCC built-in Population Count                      */
+   inline int bit_pop(uint64_t w) const { return __builtin_popcountll(w);}
+   /* Clear the Lowest set bit of given word                 */
+   inline uint64_t clear_lsb(uint64_t n)        { return (n & (n - 1));  }
+   /* Rotate all the bits set (equivalent to calling ntohl   */
+   inline uint64_t bit_propagate_ones_right(uint64_t n) const {
+      n |= n >> 1;   n |= n >> 2;    n |= n >> 4;
+      n |= n >> 8;   n |= n >> 16;   n |= n >> 32;
+      return n;
+   }
+   /* Get the Lowest set bit of given word(C built-in)       */
+   inline uint64_t get_lsb_word_pos(uint64_t n) {return __builtin_ctzll(n);}
+   /* Given position [0-n], find the 64-bit word index       */
+   inline size_t get_word_idx(size_t pos) const { return (pos >> u64_shift);}
+   /* Given a bit position [0-63) create a mask              */
+   inline uint64_t wordmask(size_t pos) const
+   { return ((uint64_t)1 << (pos % u64_bit));                            }
+   /* Given position[0-n), set/clear bit in that position    */
+   inline void set(size_t pos, bool on) {
+      if(isValid(pos)) {
+         if(on)   bw[get_word_idx(pos)] |=  wordmask(pos);
+         else     bw[get_word_idx(pos)] &= ~wordmask(pos);
+      }
+   }
+   /* Get a count of # set bits in the Bitmap                */
+   inline size_t cnt(size_t n = 0) const {
+      for(auto v : bw) n += bit_pop(v);
+      return n;
+   }
 };
 
 #endif //_BITMAP_CPP_
--- bit_ops/bitmap_test.cc
+++ bit_ops/bitmap_test.cc
@@ -1,77 +1,175 @@
+//taskset 0x1 ./bitmap_test
 #include <iostream>          /* std::cout                    */
 #include <iomanip>           /* std::setw                    */
 #include <cmath>             /* pow                          */
 #include <cassert>           /* assert                       */
 #include <algorithm>         /* std::max                     */
 
-#include "print_utils.h"     /* print_table_row              */
-#include "rand_generator.h"  /* init_rand()                  */
-#include "bitmap.h"          /* bitmap  functions            */
+#include "print_utils.h"     /* print_table_row, MILLION     */
+#include "rand_generator.h"  /* init_rand,fill_vector_rand   */
+#include "bitmap.h"          /* My Bitmap implementation     */
+#include "../../C/utils/time_api.h"  /*C based time mgmt API */
 using namespace std;
 
 /* Number of inserts == Coupon Collector problem = n ln(n)   *
  * inserts to ensure every bitmap position [1, max_rand_val] *
  * is covered                                                */
-const int number_of_inserts = 620; //~n ln(n)
 const int max_rand_val      = 128;
-const int num_test_iters    = 1;
+const int number_of_inserts = 620; //~n ln(n)
+const int num_test_iters    = 10;
+
+/* Perf test constants (container size, #ops, max set bit)   */
+static const size_t perf_size     = MILLION;
 
 /* Compare performance of STL Bitmap and my Bitmap           */
 void bitmap_perf_test()
 {
-	//unsigned long max_bit = 1000000 //Million
-	//std::bitset<max_bit> stlb;
-	//class bitmap myb(max_bit);
-	cout << "Need to implement a micro-benchmark" << endl;
+   std::vector<size_t> val(perf_size);
+   /* Fill vector with an increasing sequence 0, 1,...,10^6  */
+   std::iota (std::begin(val), std::end(val), 0);
+   //std::shuffle(val.begin(), val.end(), std::default_random_engine{});
+   struct time_api_t stl_time, my_time;
+   struct tsc_api_t stl_tsc, my_tsc;
+   
+   cout << "Running a Micro-Benchmark comparing Bitmap with STL bitset "
+        << "with a container of size " << val.size() << " and "
+        << perf_size << " operations" << endl;
+
+   /* Init code (init both clock_gettime and rdtsc           */
+   rt_measure_init(&stl_time);  rt_measure_init(&my_time);
+   tsc_measure_init(&stl_tsc);  tsc_measure_init(&my_tsc);
+
+   /* Measure STL Container first (init, set, get, clear)    */
+   rt_measure_start(&stl_time, true);
+   tsc_measure_start(&stl_tsc);
+   {
+      std::bitset<perf_size> stl_bmp;   /* create and reset  */
+      for(auto v : val) stl_bmp.set(v); /* set all bits      */
+      for(auto v : val) stl_bmp.test(v);/* get all bits      */
+      stl_bmp.reset();                  /* clear all bits    */
+   }
+   tsc_measure_end(&stl_tsc);
+   rt_measure_end(&stl_time, true);
+
+   /* Measure My Bitmap Container nxt(init, set, get, clear) */
+   rt_measure_start(&my_time, true);
+   tsc_measure_start(&my_tsc);
+   {
+      class Bitmap my_bmp(perf_size);   /* create and reset  */
+      for(auto v : val) my_bmp.set(v);  /* set all bits      */
+      for(auto v : val) my_bmp.get(v);  /* get all bits      */
+      my_bmp.reset();                   /* clear all bits    */
+   }
+   tsc_measure_end(&my_tsc);
+   rt_measure_end(&my_time, true);
+
+   /* Dump all time related statistics                       */
+   time_print_api(&stl_time, "   Info: STL Bitmap");
+   time_tsc_statistics_print(perf_size, &stl_time, &stl_tsc);
+   time_print_api(&my_time,  "   Info: My Bitmap");
+   time_tsc_statistics_print(perf_size, &my_time, &my_tsc);
 }
 
 /* Generate a sequence of random numbers. Insert them into   *
  * the bitmap and then manually check if all the bits are set*/
 bool bitmap_auto_test()
 {
-	vector<int> v(number_of_inserts);
-	class bitmap bitmap(max_rand_val);
-	
-	fill_vector_rand(v, 0, max_rand_val);
-	
-	/* Sort the random input vector and remove duplicates */
-	sort(v.begin(), v.end());
-	v.erase(unique(v.begin(), v.end()), v.end());
-	
-	cout << "Verifying bitmap @ following bit positions: ";
-	for(unsigned i = 0; i < v.size(); ++i) {
-		if(bitmap.get(v[i]) == false)
-			bitmap.set(v[i]);
-		    cout << v[i] << ", ";
-	}
-	cout << endl;
-	return true;
+   vector<uint64_t> v(number_of_inserts);
+   class Bitmap bmp(max_rand_val);
+   
+   fill_vector_rand(v, 0, max_rand_val);
+        
+   /* Sort the random input vector and remove duplicates */
+   sort(v.begin(), v.end());
+   v.erase(unique(v.begin(), v.end()), v.end());
+
+   bmp.set(v);
+   if(bmp.count() != v.size()) {
+      cout << "Error: Auto test size match issue."
+           << " Unique vector holds " << v.size() << " elements"
+           << " while bitmap hosts  " << bmp.count() <<  " elements" << endl;
+      return false;
+   }
+   for(auto val : v) {
+      if(bmp.get(val) == false) {
+         cout << "Error: Auto test get failed for " << val << endl;
+      return false;
+      }
+   }
+   return true;
 }
 
+std::vector<size_t> manual_pos = {0, 1, 2, 3, 4, 16, 25, 36, 37, 98, 99, 100, 126, 127};
+
+bool bitmap_manual_test(size_t bmp_size = 128)
+{
+   class Bitmap bmp(bmp_size);
+   if(bmp.size() < bmp_size) {
+      cout << "Error: Created bitmap is smaller than requested"
+           << "Exp=" << bmp_size << " Got=" << bmp.size() << endl;
+      return false;
+   }
+   bmp.resize(bmp_size * 2);
+   if(bmp.size() < bmp_size * 2) {
+      cout << "Error: Bitmap resize failed. "
+           << "Exp=" << bmp_size*2 << " Got=" << bmp.size() << endl;
+      return false;
+   }
+   bmp.set(manual_pos);  /* set all positions at 1 shot      */
+   if(bmp.count() != manual_pos.size()) {
+      bmp.dump();
+      cout << "Error: Bitmap Set test failed. Count not correct after set."
+           << " Exp=" << manual_pos.size() << " Got=" << bmp.count()  << endl;
+      return false;
+   }
+   /* Get test: Make sure all the bits are set               */
+   for(auto pos : manual_pos) {
+      if(bmp.get(pos) == false) {
+         cout << "Error: Bitmap Set test failed."
+              << pos << " bit not found in bitmap " << endl;
+         return false;
+      }
+   }
+   /* GetNextSet test: Verify get_next_set calculations      */
+   for(size_t i = 0; i < manual_pos.size(); ++i) {
+      size_t nxt;
+      if(i == manual_pos.size() - 1) {/* last bit is spl case*/
+         if(bmp.get_next_set_pos(manual_pos[i], nxt) == true) {
+            cout << "Error: Bitmap Get Next test failed. "
+                 << "get_next_set_pos failed for last set bit "
+                 << manual_pos[i] << " expected false but got pos = "
+                 << nxt           << endl;
+            return false;
+         }
+      }
+      else {
+         if(bmp.get_next_set_pos(manual_pos[i], nxt) == false ||
+            nxt != manual_pos[i+1]) {
+            bmp.dump();
+            cout << "Error: Bitmap Get Next test failed. "
+                 << "get_next_set_pos failed for "
+                 << manual_pos[i] << " expected=" << manual_pos[i+1]
+                 << " got="       << nxt
+                 << " bitmap_cnt="<< bmp.count() << endl;
+            return false;
+         }
+      }
+   }
+   bmp.reset();           /* reset all positions at 1 shot   */
+   if(bmp.isEmpty() == false) {
+      cout << "Error: Bitmap Clear test failed. Bitmap not empty (count="
+           << bmp.count() << ")"  << endl;
+      return false;
+   }
+   return true;
+}
 
 int main()
 {
-	init_rand();
-	bool ret = true;
-	/* Manual test-case  */
-	size_t x;
-	class bitmap bitmap(128);
-	bitmap.set(1); bitmap.set(127);
-	if(bitmap.get(1) == false)
-		cout << "Error: 1st set bit not found" <<endl;
-	else if(bitmap.get(127) == false)
-		cout << "Error: last set bit not found" <<endl;
-	/* Note, there is no elaborate test-case for             *
-	 * get_next_set_pos, as math_prime.cc does thorough test */
-	else if(bitmap.get_next_set_pos(1, x) == false || x != 127)
-		cout << "Error: get_next_set_pos failed. Expecting 127, got "
-			 << x << endl;
-	else {
-		cout << "Info: All manual test-cases passed" << endl;
-		/* Auto test cases   */
-		for(auto i = 0; i < num_test_iters && ret; ++i)
-			ret = bitmap_auto_test();
-		//if(ret)
-		//bitmap_perf_test();
-	}
+   init_rand();
+   if(bitmap_manual_test() == false)     return -1;
+   for(auto i = 0; i < num_test_iters; ++i)
+      if(bitmap_auto_test() == false) return -1;
+   cout << "Info: All manual and randomized test-cases passed" << endl;
+   bitmap_perf_test();
 }
