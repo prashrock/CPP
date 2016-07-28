442d9550cd24af88c0ea78e7939683ecb793b5e7
Prashanth Kannan <prashrock@gmail.com>
2016-07-28 09:29:01 -0700

    Use Bitmap class in Eratosthenes

--- math/math_prime.h
+++ math/math_prime.h
@@ -1,21 +1,20 @@
 #ifndef _MATH_PRIME_CPP
 #define _MATH_PRIME_CPP
 #include <iostream>          /* std::cout                    */
-#include <iomanip>           /* std::setw                    */
 #include <cmath>             /* pow                          */
 #include <cassert>           /* assert                       */
 #include <algorithm>         /* std::max                     */
 
-#include "bitmap.h"          /* bitmap  functions            */
+#include "bitmap.h"          /* My bitmap functions          */
 
 template<size_t N>
 void prime_seive_eratosthenes(size_t primes[N],
                               size_t max_rand_val)
 {
    int i;
-   bitmap seive(max_rand_val);
-   seive.set_all(); /* Start with all #'s as valid prime    */
-   /* Identify each prime, clear all its multiples          */
+   class Bitmap seive(max_rand_val);
+   seive.setAll(); /* Start with all #'s as valid prime      */
+   /* Identify each prime, clear all its multiples           */
    for(size_t i = 2; i < max_rand_val; ++i) {
       if(seive.get(i) == false) continue;
       for(size_t j = i; j <= max_rand_val/i; ++j)
---
+++ math/math_stat.h
@@ -0,0 +1,20 @@
+#ifndef _MATH_STAT_CPP
+#define _MATH_STAT_CPP
+#include <iostream>          /* std::cout                    */
+#include <cmath>             /* pow                          */
+#include <algorithm>         /* std::max                     */
+
+// TODO !!
+
+/*
+ * Compute the following parameters given a vector
+ * Mean
+ * Median
+ * Std Dev
+ * 95th percentile
+ * 99th percentile
+ * max
+ */
+
+
+#endif //_MATH_STAT_CPP
