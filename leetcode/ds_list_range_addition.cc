2b6d3fea92b8c5e19e523abf191b101b35833c91
Prashanth Kannan <prashrock@gmail.com>
2016-08-07 15:47:59 -0700

    Makefile cleanup

---
+++ leetcode/ds_list_range_addition.cc
@@ -0,0 +1,36 @@
+//g++ --std=c++11 -Wall -g -o ds_list_range_addition ds_list_range_addition.cc
+
+/**
+ * @file  Range Addition
+ * @brief k update operations onto array
+ */
+
+// https://leetcode.com/problems/range-addition/
+
+#include <iostream>          /* std::cout                    */
+#include <algorithm>         /* std::max                     */
+#include <unordered_map>     /* std::unordered_map container */
+using namespace std;
+
+/**
+ * Assume you have an array of length n initialized with all 0's
+ * and are given k update operations.
+ * Each operation is represented as a triplet: [startIndex,endIndex,inc]
+ * which increments each element of subarray A[startIndex...endIndex]
+ * (startIndex and endIndex inclusive) with inc.
+ * Return the modified array after all k operations were executed.
+ * Example:
+ * Given:
+ * length = 5,
+ * updates = [ [1,  3,  2],
+ *             [2,  4,  3],
+ *             [0,  2, -2] ]
+ * Output:  [-2, 0, 3, 5, 3]
+ */
+
+
+int main()
+{
+   cout << "Info: Need to add manual tests for merge sorted arrays" << endl;
+   return 0;
+}
