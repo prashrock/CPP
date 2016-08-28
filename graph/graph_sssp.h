ca9fd16f72f795591dd3a6f8d4fd39dfd65911b4
Prashanth Kannan <prashrock@gmail.com>
2016-08-28 06:16:11 -0700

    Dijkstra SSSP implementation

--- graph/graph_sssp.h
+++ graph/graph_sssp.h
@@ -10,6 +10,25 @@
 namespace graph
 {
 
+/* Create a custom comparator without using any classes      */
+struct less_than_start {
+
+};
+   
+struct neighbor {
+   int to;                      /* Next node (outgoing edge) */
+   neighbor(int v) : to(v) {}
+};
+
+struct neighbor_wt {
+   int to;                      /* Next node (outgoing edge) */
+   int wt;
+   neighbor_wt(int v, int w) : to(v), wt(w) {}
+   inline bool operator <(const neighbor_wt& other) const {
+      return (wt < other.wt);
+   }
+};
+   
 /** Single Source Shortest Path Algorithms:                  *
  * Given a graph G=(V,E) and source vertex s, find the min   *
  * cost path from s to every vertex in V.                    */
@@ -17,9 +36,44 @@ namespace graph
 /** BFS for non-weighted graphs.                             */
    
 /** Dijkstra Shortest Path Greedy Algo (Heap based approach) *
- * for weighted graphs with non-negative edges.              *
+ * for weighted graphs with non-negative edges               *
+ * Below is the algorithm:                                   *
+ * - Initialize min_dist for all nodes as MAX_INT (infinite) *
+ * - Use an 'active' set(min_heap) of yet to visit nodes     *
+ * - Add source node with distance 0 to this list            *
+ * - While 'active' set is no empty                          *
+ *   - Get the first node in active set (smallest distance)  *
+ *   - For each neighbor of this node, calculate new distance*
+ *   - If (known dist > new distance) : add to active set    *
  * Time Complexity = O(m lg n), m=#edges, n=#vertices        */
+int dijkstra_sssp(int src, int dst,
+                  const std::vector<std::vector<neighbor_wt>>& graph) {
+   std::vector<int> min_distance(graph.size(),
+                                  std::numeric_limits<int>::max());
+   min_distance[src] = 0;   /* src can reach src in 0 dist   */
+   std::set<neighbor_wt> active_vertices; /* unknown frontier*/
+   active_vertices.insert({src, 0}); /* add src to frontier  */
 
+   while(active_vertices.empty() == false) {
+      /* Pick the next smallest vertex from unknown frontier */
+      int where = active_vertices.begin()->to;
+      /* End the search if destination vertex is reached.    *
+       * Remove this if to find shortest dist to all vertexes*/
+      if(where == dst) return min_distance[dst];
+      active_vertices.erase(active_vertices.begin());
+      for(auto ed : graph[where]) { /* Visit all neighbors   */
+         /* If any neighbor has shorter cost, add to frontier*/
+         if(min_distance[ed.to] > min_distance[where] + ed.wt) {
+            active_vertices.erase( {ed.to, min_distance[ed.to]} );
+            min_distance[ed.to] = min_distance[where] + ed.wt;
+            active_vertices.insert( {ed.to, min_distance[ed.to]} );
+         }
+      }
+   }
+   /*If shortest path from src->dst does not exceed, ret max */
+   return std::numeric_limits<int>::max();
+}
+   
 /** Floyd-Warshall All path shortest-pair Algo               *
  * Note - find shortest path from all sources to all targets *
  * Note - can also find the connectivity (transitive closure)*
--- graph/graph_test.cc
+++ graph/graph_test.cc
@@ -8,14 +8,32 @@
 
 #include "rand_generator.h"    /* init_rand()                */
 #include "graph.h"             /* Undirected/Directed Graphs */
-#include "print_utils.h"       /*print_table_row             */
+#include "graph_sssp.h"        /* Dijkstra's SSSP            */
+#include "print_utils.h"       /* print_table_row            */
 using namespace std;
 using namespace graph;
 
+bool dijkstra_test() {
+   /* ex from http://www.ifp.illinois.edu/~angelia/ge330fall09_dijkstra_l18.pdf    */
+   std::vector<std::vector<neighbor_wt>> graph = {
+      {},
+      {{2, 7}, {3, 9}, {6, 14}},
+      {{3, 10}, {4, 15}},
+      {{4, 11}, {6, 2}},
+      {{5, 6}},
+      {},
+      {{5, 9}},
+   };
+   if     (dijkstra_sssp(1, 2, graph) != 7)  return false;
+   else if(dijkstra_sssp(1, 6, graph) != 11) return false;
+   else if(dijkstra_sssp(1, 4, graph) != 20) return false;
+   else                                      return true;
+}
 
 int main()
 {
    init_rand();
+   if(dijkstra_test() == false) return -1;
    cout << "Info: Manual tests passed successfully" << endl;
    return 0;
 }
